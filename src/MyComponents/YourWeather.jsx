3b1b7a9171781a17933ac847928e659d0efd96d7
kashifmalik962 <kashifmalik962@gmail.com>
2024-12-21 16:33:44 +0530

    fix bug of image in your location

--- src/MyComponents/YourWeather.jsx
+++ src/MyComponents/YourWeather.jsx
@@ -4,6 +4,7 @@ import "./YourWeather.css";
 import { useContext } from "react";
 import MyContext from '../MyContext';
 import { useNavigate } from "react-router-dom";
+import image from "../assets/location.png"
 
 const YourWeather = () => {
    const navigate = useNavigate();
@@ -98,7 +99,7 @@ const YourWeather = () => {
 
   return (
     <div className="your-Weather">
-      <img src="src/assets/location.png" width={"80px"} height={"80px"} alt="Location Image" />
+      <img src={image} width={"80px"} height={"80px"} alt="Location Image" />
       <p className="p1-your-weather">Grant Location Access</p>
       <p className="p2-your-weather">Allow Access to get weather Information</p>
       <button className="btn-grand-access" onClick={getLocation}>
